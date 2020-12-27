38eb4019603dbacc748d87b399fa59ac8c9713fd
Artem <artver1257@gmail.com>
2020-12-27 18:15:35 +0300

    Update 03.py

--- 03.py
+++ 03.py
@@ -6,6 +6,6 @@ print('Hello', user_name, "in your name"), len(user_name), 'letters'
 
 user_year =int(input("When you was born?"))
 if 2020 - user_year >= 18:
-    print(user_name+ 'you can watch a action movie')
+    print(user_name+ 'you can watch a horror movie')
 else:
     print(user_year- 'you can watch a comedy')
