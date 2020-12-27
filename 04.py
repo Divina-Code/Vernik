55d0b2187f245507a664dd7a9294f73c8faccaec
Artem <artver1257@gmail.com>
2020-12-27 18:02:05 +0300

    Create 04.py

---
+++ 04.py
@@ -0,0 +1,13 @@
+print("Hello user")
+
+user_name = input("What is your name?")
+
+print('Hello', user_name, "in your name"), len(user_name), 'letters'
+
+user_year =int(input("When you was born?"))
+if len(str(user_year)) ==4:
+    print("nice year,", user_year)
+    print(2020-user_year)
+else:
+    print("You make a mistake")
+    print("good bye")
