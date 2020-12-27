9b8fc71b14c50b623360f671a4b32c1a9d9da278
Artem <artver1257@gmail.com>
2020-12-27 16:27:24 +0300

    Update 02. How are you.py

--- 02. How are you.py
+++ 02. How are you.py
@@ -2,10 +2,9 @@ print("Hello, user!")
 
 user_name = input("What is your name?")
 
-print("Hello, user_name")
+print('Hello', user_name, "in your name", len(user_name), 'letters')
 
 user_year = int( input("When you was born?"))
+print(len(str(user_year)) == 4)
 print("nice year,", user_year)
-
-
 print(2020-user_year)
